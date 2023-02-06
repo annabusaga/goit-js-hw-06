99b5c2f3af6f97c266c2cc0f78a2726b4eba7ee2
annabusaga <anna.busaga@gmail.com>
2023-02-06 12:06:13 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+
+const listRef = document.querySelector('[id="ingredients"]')
+
+const array = [];
+for (const ingredient of ingredients) {
+  const newItem = document.createElement('li')
+  newItem.textContent = ingredient
+  newItem.classList.add('item')
+  array.push(newItem);
+}
+listRef.append(...array)
+
+
